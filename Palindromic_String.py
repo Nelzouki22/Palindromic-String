11dfc73c6da1bde55dd761b29b2d923456fd67e4
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-09-06 21:12:18 -0400

    Add project files.

---
+++ Palindromic_String.py
@@ -0,0 +1,15 @@
+﻿def is_palindrome(s):
+    # إزالة المسافات وتحويل السلسلة إلى أحرف صغيرة
+    s = s.replace(" ", "").lower()
+    # مقارنة السلسلة بالعكس
+    return s == s[::-1]
+
+# قراءة الإدخال من المستخدم
+input_string = input()
+
+# التحقق من كون السلسلة باليندروم
+if is_palindrome(input_string):
+    print("YES")
+else:
+    print("NO")
+
